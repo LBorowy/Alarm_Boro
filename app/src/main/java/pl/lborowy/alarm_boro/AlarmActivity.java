653d4b581ae5a6de99974fc416a5d54377b3d055
Łukasz Borowy <lucas.borowy@gmail.com>
2017-08-07 21:01:39 +0200

    Alarm Activity + vibrate/stop

--- app/src/main/java/pl/lborowy/alarm_boro/AlarmActivity.java
+++ app/src/main/java/pl/lborowy/alarm_boro/AlarmActivity.java
@@ -4,22 +4,37 @@ import android.content.Context;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlarmActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 
-        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         if (Build.VERSION.SDK_INT >= 26) {
             vibrator.vibrate(VibrationEffect.createOneShot(2000, 1000));
         } else {
-            long[] pattern = {0, 100, 1000};
+            long[] pattern = {0, 1000, 100}; // czekanie na wibracje / jak dlugo wibruje / powtarzalnosc
             vibrator.vibrate(pattern, 0); // 0 - powtarza cały czas
         }
+
+        button = (Button) findViewById(R.id.alarm_activity_stop);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibrator.cancel();
+            }
+        });
     }
+
+
 }
