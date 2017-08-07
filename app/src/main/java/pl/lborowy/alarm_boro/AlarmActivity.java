456df7be63b9f4ac335e9305233756195824bff1
Łukasz Borowy <lucas.borowy@gmail.com>
2017-08-07 20:52:15 +0200

    Alarm Activity + vibrate

--- app/src/main/java/pl/lborowy/alarm_boro/AlarmActivity.java
+++ app/src/main/java/pl/lborowy/alarm_boro/AlarmActivity.java
@@ -1,5 +1,9 @@
 package pl.lborowy.alarm_boro;
 
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,13 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= 26) {
+            vibrator.vibrate(VibrationEffect.createOneShot(2000, 1000));
+        } else {
+            long[] pattern = {0, 100, 1000};
+            vibrator.vibrate(pattern, 0); // 0 - powtarza cały czas
+        }
     }
 }
