888cfe3cf6211bd8a64b9caefcd6352a0dea8fab
Łukasz Borowy <lucas.borowy@gmail.com>
2017-08-08 19:43:27 +0200

    setAlarm/testAlarm refactor

--- app/src/main/java/pl/lborowy/alarm_boro/MainActivity.java
+++ app/src/main/java/pl/lborowy/alarm_boro/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,10 @@ import android.widget.Button;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PendingIntent pendingIntent;
+    private AlarmManager alarmManager;
+    private Intent startAlarmIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,36 +25,24 @@ public class MainActivity extends AppCompatActivity {
         TimePicker timePicker = (TimePicker) findViewById(R.id.main_activity_time_picker);
         timePicker.setIs24HourView(true);
 
-        final Intent startAlarmIntent = new Intent(this, AlarmActivity.class);
 
-        Button testButton = (Button) findViewById(R.id.main_activity_test_alarm);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Intent startAlarmIntent = new Intent(MainActivity.this, AlarmActivity.class);
-                startActivity(startAlarmIntent);
-            }
-        });
-
-
+        startAlarmIntent = new Intent(this, AlarmActivity.class);
         // jak znajdzie pending z kodem 0, to go podmieni (aplikacja przyjmuje kod 1)
-        final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, startAlarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
+        pendingIntent = PendingIntent.getActivity(this, 0, startAlarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         // alarm manager - zarządza alarmami (usługa systemowa)
-        final AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        Button setAlarmButton = (Button) findViewById(R.id.main_activity_set_alarm);
-        setAlarmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // zarządzanie alarmami
-                alarmManager.set(
-                        AlarmManager.RTC_WAKEUP, // czas rzeczywisty + wybudzenie
-                        System.currentTimeMillis() + 100,
-                        pendingIntent);
-            }
-        });
     }
 
+    public void setAlarm(View view) {
+        // zarządzanie alarmami
+        alarmManager.set(
+                AlarmManager.RTC_WAKEUP, // czas rzeczywisty + wybudzenie
+                System.currentTimeMillis() + 100,
+                pendingIntent);
+    }
 
+    public void runAlarm(View view) {
+        startActivity(startAlarmIntent);
+    }
 }
