537adc4eb2c83b8f5fe12193cad78c3cb1af1cd1
Łukasz Borowy <lucas.borowy@gmail.com>
2017-08-08 18:20:03 +0200

    test alarm vibrate/stop - refactor

--- app/src/main/java/pl/lborowy/alarm_boro/AlarmActivity.java
+++ app/src/main/java/pl/lborowy/alarm_boro/AlarmActivity.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,47 +17,62 @@ import android.widget.Button;
 public class AlarmActivity extends AppCompatActivity {
 
     private Button button;
+    private Vibrator vibrator;
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 
-        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
         View mainView = findViewById(R.id.activity_alarm_view);
+        View stopAlarmButton = findViewById(R.id.alarm_activity_stop);
 
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        animation = setupAnimation();
+
+        startVibration(vibrator);
+        mainView.startAnimation(animation);
+
+
+        stopAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibrator.cancel();
+            }
+        });
+
+        stopAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopAlarm();
+            }
+        });
+    }
+
+    private void stopAlarm() {
+        vibrator.cancel();
+        animation.cancel();
+        finish();
+    }
+
+    private void startVibration(Vibrator vibrator) {
         if (Build.VERSION.SDK_INT >= 26) {
             vibrator.vibrate(VibrationEffect.createOneShot(2000, 1000));
         } else {
             long[] pattern = {0, 1000, 100}; // czekanie na wibracje / jak dlugo wibruje / powtarzalnosc
             vibrator.vibrate(pattern, 0); // 0 - powtarza cały czas
         }
+    }
 
-        button = (Button) findViewById(R.id.alarm_activity_stop);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                vibrator.cancel();
-            }
-        });
-
+    @NonNull
+    private Animation setupAnimation() {
         final Animation animation = new AlphaAnimation(1, 0);
         animation.setInterpolator(new LinearInterpolator()); // zawsze musi być
         animation.setRepeatCount(Animation.INFINITE); // powtarzalnośc /do końca/
         animation.setRepeatMode(Animation.REVERSE); // ciemno-jasno-jasno-ciemno
         animation.setDuration(500);
-        mainView.startAnimation(animation);
-
-        View stopAlarmButton = findViewById(R.id.alarm_activity_stop);
-        stopAlarmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                vibrator.cancel();
-                animation.cancel();
-                finish();
-            }
-        });
+        return animation;
     }
 
 
